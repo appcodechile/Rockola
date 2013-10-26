db8abc40fd6524f866b4293bfc9ff44f95caca8c
Claudio Cárcamo Correa <ccarcamocorrea@gmail.com>
2013-10-25 21:11:23 -0300

    otro mas

--- webapp/app.js
+++ webapp/app.js
@@ -5,13 +5,8 @@ Ext.Loader.setPath({
 //</debug>
 
 /**
- * This example demonstrates the Carousel component in Sencha Touch 2.
- *
- * The carousel can run both horizontally and vertically, and in this example in combine both
- * of them together.
- *
- * The final result will be 4 horizontal carousels inside 1 vertical carousel.
- * Each of the horizontal carousels will have a category of images within it.
+/*
+ Rockola
  */
 Ext.application({
     startupImage: {
@@ -31,18 +26,13 @@ Ext.application({
         114: 'resources/icons/icon@2x.png',
         144: 'resources/icons/icon@144.png'
     },
-
-    //here we require any components we are using in our application
     requires: [
         'Ext.carousel.Carousel',
         'Ext.Img',
         'Ext.device.Device'
     ],
 
-    /**
-     * The launch method is called when the browser is ready and the application is ready to
-     * launch.
-     */
+    /**Cargamos la App*/
     launch: function() {
     	
         var categories = ['intro'],
@@ -65,32 +55,27 @@ Ext.application({
 
             horizontalCarousels.push({
                 xtype: 'carousel',
-                //the direction is horizontal
                 direction: 'horizontal',
-
-                //we turn on direction lock so you cannot scroll diagonally
                 directionLock: true,
                 flex: Ext.os.is.Phone ? 5 : 6,
                         config: {
                             fullscreen: true
                         },
 
-                //and give it the items array
                 items: items
             });
         }
 
-        //and finally we create the vertical carousel which contains each of the horizontal
-        //category carousels above
+        
         Ext.Viewport.add({
             xtype: 'carousel',
             fullscreen: true,
             bufferSize: 2,
 
-            //this time direction vertical
+            
             direction: 'vertical',
 
-            //and the horizontalCarousels array
+            
             items: horizontalCarousels
         });
     }
